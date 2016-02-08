0c938e53547fc987c25c5aea7f3d1f6194819199
TheBurnerGuy <henryweilo@hotmail.com>
2016-02-07 22:51:49 -0700

    Update Graph

--- assignment1.py
+++ assignment1.py
@@ -47,6 +47,49 @@ class Graph:
         self._alist = {}
         self._coord = {}
 
+    def add_vertex (self, vertex):
+        ''' Adds 'vertex' to the graph
+        Preconditions: None
+        Postconditions: self.is_vertex(vertex) -> True
+        '''
+        if vertex not in self._alist:
+            self._alist[vertex] = set()
+
+    def add_edge (self, source, destination):
+        ''' Adds the edge (source, destination)
+        Preconditions: None
+        Postconditions:
+        self.is_vertex(source) -> True,
+        self.is_vertex(destination),
+        self.is_edge(source, destination) -> True
+        '''
+        self.add_vertex(source)
+        self.add_vertex(destination)
+        self._alist[source].add(destination)
+
+    def is_edge (self, source, destination):
+        '''Checks whether (source, destination) is an edge
+        '''
+        return (self.is_vertex(source)
+                and destination in self._alist[source])
+
+    def is_vertex (self, vertex):
+        '''Checks whether vertex is in the graph.
+        '''
+        return vertex in self._alist
+
+    def neighbours (self, vertex):
+        '''Returns the set of neighbours of vertex. DO NOT MUTATE
+        THIS SET.
+        Precondition: self.is_vertex(vertex) -> True
+        '''
+        return self._alist[vertex]
+
+    def vertices (self):
+        '''Returns a set-like container of the vertices of this
+        graph.'''
+        return self._alist.keys()
+
 class WeightedGraph (Graph):
     '''A weighted graph stores some extra information (usually a
     "weight") for each edge.'''
